f7a4df9198b71ba2e22bdbf83e0731fa3c387b7d
dekimioc <dekimioc@yahoo.com>
2019-09-23 22:15:37 +0200

    Component breaked into smaller parts and maped into main component

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import { Card } from '../Card/Card';
+
 import './CardList.style.css';
 
 const CardList = (props) => {
     return(
-        <div className="card-list">{props.children}</div>
+        <div className="card-list"> 
+            {props.monsters.map(monster => 
+                <Card  key={monster.id} monster={monster}/>)}
+        </div>
     )
 }
 

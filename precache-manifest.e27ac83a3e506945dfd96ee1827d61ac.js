ffff0439dda8eef58caa855d536a9ae45b9ec38e
Dejan Miocinovic <dekimioc@yahoo.com>
2020-02-17 13:53:43 +0100

    Updates

---
+++ precache-manifest.e27ac83a3e506945dfd96ee1827d61ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba491a5c96a94e0a86ee3802d18923a4",
+    "url": "/monsters-app/index.html"
+  },
+  {
+    "revision": "17b53084ac9a9c12d406",
+    "url": "/monsters-app/static/css/main.da763ef3.chunk.css"
+  },
+  {
+    "revision": "e260c628452a8e3fa795",
+    "url": "/monsters-app/static/js/2.cdcd8c14.chunk.js"
+  },
+  {
+    "revision": "17b53084ac9a9c12d406",
+    "url": "/monsters-app/static/js/main.2f6e0d26.chunk.js"
+  },
+  {
+    "revision": "9eb6450b7e23d85ce858",
+    "url": "/monsters-app/static/js/runtime-main.1a7cfa18.js"
+  }
+]);
